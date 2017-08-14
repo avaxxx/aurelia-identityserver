f906ad9f3d62932019dbb9ffef02b9a8ee36b806
avaxxx <avaxxx@gmail.com>
2017-08-14 19:28:32 +0200

    Fix dependencies

--- IdentityServer/Config.cs
+++ IdentityServer/Config.cs
@@ -66,7 +66,7 @@ namespace AuAuth.IdentityServer
                     ClientName = "MVC Client",
                     AllowedGrantTypes = GrantTypes.HybridAndClientCredentials,
 
-                    RequireConsent = true,
+                    RequireConsent = false,
 
                     ClientSecrets = 
                     {
