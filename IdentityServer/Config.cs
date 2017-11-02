ca05151e2daa7283bdb7055884d9d470246e6efd
avaxxx <avaxxx@gmail.com>
2017-11-02 21:50:18 +0100

    Changes

--- IdentityServer/Config.cs
+++ IdentityServer/Config.cs
@@ -5,6 +5,13 @@ using IdentityServer4;
 using IdentityServer4.Models;
 using System.Collections.Generic;
 using System.Security.Claims;
+using IdentityServer4.Services;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Threading.Tasks;
+using IdentityServer4.Extensions;
+using System.Linq;
+using AuAuth.Database.Entities;
 
 namespace AuAuth.IdentityServer
 {
@@ -28,6 +35,8 @@ namespace AuAuth.IdentityServer
                 new ApiResource("api1", "My API")
             };
         }
+
+        
 
         // clients want to access resources (aka scopes)
         public static IEnumerable<Client> GetClients()
@@ -108,4 +117,42 @@ namespace AuAuth.IdentityServer
             };
         }
     }
+     public class IdentityProfileService : IProfileService
+    {
+
+        private readonly IUserClaimsPrincipalFactory<User> _claimsFactory;
+        private readonly UserManager<User> _userManager;
+
+        public IdentityProfileService(IUserClaimsPrincipalFactory<User> claimsFactory, UserManager<User> userManager)
+        {
+            _claimsFactory = claimsFactory;
+            _userManager = userManager;
+        }
+
+        public async Task GetProfileDataAsync(ProfileDataRequestContext context)
+        {
+            var sub = context.Subject.GetSubjectId();
+            var user = await _userManager.FindByIdAsync(sub);
+            if (user == null)
+            {
+                throw new ArgumentException("");
+            }
+
+            var principal = await _claimsFactory.CreateAsync(user);
+            var claims = principal.Claims.ToList();
+
+            //Add more claims like this
+            claims.Add(new System.Security.Claims.Claim("MyProfileID", user.Id));
+
+            context.IssuedClaims = claims;
+        }
+
+        public async Task IsActiveAsync(IsActiveContext context)
+        {
+            var sub = context.Subject.GetSubjectId();
+            var user = await _userManager.FindByIdAsync(sub);
+            context.IsActive = user != null;
+        }
+    }
+
 }
--- Startup.cs
+++ Startup.cs
@@ -54,6 +54,7 @@ namespace WebApplicationBasic
                 .AddInMemoryIdentityResources(Config.GetIdentityResources())
                 .AddInMemoryApiResources(Config.GetApiResources())
                 .AddInMemoryClients(Config.GetClients())
+                .AddProfileService<IdentityProfileService>()
                 .AddAspNetIdentity<User>();
 
             // Add external authentication middleware below. To configure them please see http://go.microsoft.com/fwlink/?LinkID=532715
@@ -72,25 +73,25 @@ namespace WebApplicationBasic
                 options.DefaultScheme = "Cookies";
                 options.DefaultChallengeScheme = "oidc";
             })
-                .AddCookie("Cookies")
-                .AddOpenIdConnect("oidc", options =>
-                {
-                    options.SignInScheme = "Cookies";
+                .AddCookie("Cookies");
+                // .AddOpenIdConnect("oidc", options =>
+                // {
+                //     options.SignInScheme = "Cookies";
 
-                    options.Authority = "http://localhost:5000";
-                    options.RequireHttpsMetadata = false;
+                //     options.Authority = "http://localhost:5000";
+                //     options.RequireHttpsMetadata = false;
 
-                    options.ClientId = "mvc";
-                    options.ClientSecret = "secret";
-                    options.ResponseType = "code id_token";
+                //     options.ClientId = "mvc";
+                //     options.ClientSecret = "secret";
+                //     options.ResponseType = "code id_token";
 
-                    options.SaveTokens = true;
-                    options.GetClaimsFromUserInfoEndpoint = true;
+                //     options.SaveTokens = true;
+                //     options.GetClaimsFromUserInfoEndpoint = true;
 
-                    options.Scope.Add("api1");
-                    options.Scope.Add("offline_access");
-                    options.Scope.Add("role");
-                });
+                //     options.Scope.Add("api1");
+                //     options.Scope.Add("offline_access");
+                // })
+                // .AddOpenIdConnect();
 
             //services.UseCookieAuthentication(new CookieAuthenticationOptions
             //{
